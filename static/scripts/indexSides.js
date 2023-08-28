6a0be4189a119fe5ea4fba00d860fd9a38992ccc
Marcel <marcel.piotrowski5543@gmail.com>
2023-08-28 16:29:36 +0200

    fix: svgs introduced instead of 'triangle' divs

--- static/scripts/indexSides.js
+++ static/scripts/indexSides.js
@@ -1,43 +1,47 @@
 const leftSide = document.getElementById("left-side");
 const rightSide = document.getElementById("right-side");
 const container = document.getElementById("sides-container");
-//todo: snapback
+const topLeft = leftSide.style.top;
+const topRight = rightSide.style.top;
 
 function isSmallScreen() {
     return window.matchMedia("(max-width: 1600px)").matches;
 }
 
-function showSides() {
+//todo: function isMobile
+
+function checkScroll() {
+    //todo: stay on refresh
     let scrollTop = document.documentElement.scrollTop;
     let isSmall = isSmallScreen()
-    //todo: let isMobile = isMobileScreen()
-    if(scrollTop > (isSmall ? 400 : 600)) {
-        rightSide.classList.add('lay-right');
-        leftSide.classList.add('lay-left');
-        rightSide.classList.add('easy-transition');
-        leftSide.classList.add('easy-transition');
-        leftSide.classList.remove('slide-from-left');
-        rightSide.classList.remove('slide-from-right');
+    //todo: let isMobile
+    if(scrollTop >= (isSmall ? 400 : 600)) {
+        leftSide.classList.remove('sides-stay');
+        leftSide.classList.add('sides-show');
+        rightSide.classList.remove('sides-stay');
+        rightSide.classList.add('sides-show');
+    }
+    else if(scrollTop >= (isSmall ? 350 : 550)){
+        leftSide.classList.remove('sides-fix');
+        leftSide.classList.add('sides-stay');
+        rightSide.classList.remove('sides-fix');
+        rightSide.classList.add('sides-stay');
+        leftSide.style.top = '80%';
+        rightSide.style.top = '90%';
     }
     else {
-        leftSide.classList.remove('lay-left')
-        rightSide.classList.remove('lay-right')
-        leftSide.classList.remove('easy-transition');
-        rightSide.classList.remove('easy-transition');
-        leftSide.classList.remove('slide-from-left');
-        rightSide.classList.remove('slide-from-right');
-    }
-    if(scrollTop > (isSmall ? 450 : 650)) {
-        leftSide.classList.add('slide-from-left');
-        rightSide.classList.add('slide-from-right');
+        leftSide.classList.remove('sides-show');
+        leftSide.classList.add('sides-fix');
+        rightSide.classList.remove('sides-show');
+        rightSide.classList.add('sides-fix');
+        leftSide.style.top = topLeft;
+        rightSide.style.top = topRight;
     }
 }
-        // leftSide.style.right = (isSmall ? '40%' : '75%');
-        // rightSide.style.left = (isSmall ? '65%' : '83%');
 
 function init() {
-    window.addEventListener('scroll', showSides);
-    window.addEventListener('resize', showSides)
+    window.addEventListener('scroll', checkScroll);
+    window.addEventListener('resize', checkScroll)
 }
 
 window.addEventListener('DOMContentLoaded', init);
