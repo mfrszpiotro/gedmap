f9030ad8beffa32dd396f2eb4947c6b8e58a20e4
Marcel <marcel.piotrowski5543@gmail.com>
2023-09-08 15:37:42 +0200

    design: mobile navbar sliding down and link hovers

--- static/scripts/hideNav.js
+++ static/scripts/hideNav.js
@@ -1,17 +1,41 @@
 const navbar = document.getElementById("navbar");
+const hamburg = document.getElementById("trigger-mobile-nav")
+const hamburgIcon = document.getElementById("hamburg-icon")
+let isHamburg = true;
 
 function hideNav() {
     let scrollTop = window.scrollY || document.documentElement.scrollTop;
-    if(scrollTop > 50) {
-        navbar.style.top = '-400px';
+    if (scrollTop > 50) {
+        navbar.classList = ("all-hide");
     }
     else {
-        navbar.style.top='-100px';
+        navbar.classList.remove("all-hide");
     }
+}
+
+function overlayNav() {
+    if (isHamburg) {
+        navbar.classList.add('mobile-show');
+        hamburgIcon.textContent = "X";
+        isHamburg = false;
+    }
+    else {
+        navbar.classList.remove('mobile-show');
+        hamburgIcon.textContent = "\u2630";
+        isHamburg = true;
+    }
+}
+
+function fixOverlayNav() {
+    navbar.classList.remove('mobile-show');
+    hamburgIcon.textContent = "\u2630";
+    isHamburg = true;
 }
 
 function init() {
     window.addEventListener('scroll', hideNav);
+    window.addEventListener('resize', fixOverlayNav);
+    hamburg.addEventListener('click', overlayNav);
 }
 
 window.addEventListener('DOMContentLoaded', init);
